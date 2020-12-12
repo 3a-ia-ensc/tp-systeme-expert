563c04fdd8c3b92f8215d2a3bc5ad1a5d6b7cbef
saudrix <saudrix@gmail.com>
2020-12-12 18:00:17 +0100

    going crazy with articles

--- src/main.py
+++ src/main.py
@@ -65,6 +65,94 @@ def initialize_akinn():
                      year='1993',
                      url='https://www.sciencedirect.com/science/article/abs/pii/003132039390135J'))
 
+    art_3 = Fact('article-mlp',
+                 Article(
+                    name='Multilayer Perceptron Tutorial',
+                    authors='Leonardo Noregia',
+                    year='2005',
+                    url='http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.608.2530&rep=rep1&type=pdf'
+                 ))
+
+    art_4 = Fact('article-cnn',
+                 Article(
+                    name='An Introduction to Convolutional Neural Networks',
+                    authors='Keiron O\'Shea, Ryan Nash',
+                    year='2015',
+                    url='https://arxiv.org/abs/1511.08458'
+                 ))
+
+    art_5 = Fact('article-seg-cnn',
+                 Article(
+                    name='DRINet for Medical Images Segmentation',
+                    authors='Liand Chen, Pzul Bentley, Kensaku Mori et al.',
+                    year='2018',
+                    url='https://ieeexplore.ieee.org/abstract/document/8357580'
+                 ))
+
+    art_6 = Fact('article-img-compl-cnn',
+                 Article(
+                    name='Globally and locally consistent image completion',
+                    authors='Satoshi lizuka, Edgar Simo-Serra, Iroshi Ishikawa',
+                    year='2017',
+                    url='https://dl.acm.org/doi/abs/10.1145/3072959.3073659'
+                 ))
+
+    art_7 = Fact('article-img-compr-vae',
+                 Article(
+                    name='Efficient image encryption and compression based on a VAE generative model',
+                    authors='Xintao Duan, Jinjing Liu, En Zhang',
+                    year='2019',
+                    url='https://link.springer.com/article/10.1007/s11554-018-0826-4'
+                 ))
+
+    art_8 = Fact('article-img-gen-rec-sup-vae',
+                 Article(
+                    name='Generating Diverse High-Fidelity Images with VQ-VAE-2',
+                    authors='Ali Razavi, Aaron van den Oord, Oriol Vinyals',
+                    year='2019',
+                    url='https://proceedings.neurips.cc/paper/2019/hash/5f8e2fa1718d1bbcadf1cd9c7a54fb8c-Abstract.html'
+                 ))
+
+    art_9 = Fact('article-img-gen-rec-low-vae',
+                 Article(
+                    name='Understanding disentangling in β-VAE',
+                    authors='Christopher P. Burgess, Irina Higgins, Arka Pal, Loic Matthey, Nick Watters, Guillaume Desjardins, Alexander Lerchner',
+                    year='2018',
+                    url='https://arxiv.org/abs/1804.03599'
+                 ))
+
+    art_10 = Fact('article-img-gen-gan',
+                 Article(
+                    name='GAN-based synthetic brain MR image generation',
+                    authors='Changhee Han, Hideaki Hayashi, Leonardo Rundo, Ryosuke Araki, Wataru Shimoda, Shinichi Muramatsu, Yujiro Furukawa',
+                    year='2018',
+                    url='https://ieeexplore.ieee.org/abstract/document/8363678'
+                 ))
+
+    art_11 = Fact('article-son-class-cnn',
+                 Article(
+                    name='Deep Convolutional Neural Networks and Data Augmentation for Environmental Sound Classification',
+                    authors='Justin Salamon, Juan Pablo Bello',
+                    year='2017',
+                    url='https://ieeexplore.ieee.org/abstract/document/7829341'
+                 ))
+
+    art_12 = Fact('article-son-seg-cnn',
+                 Article(
+                    name='A study of time-frequency features for CNN-based automatic heart sound classification for pathology detection',
+                    authors='Baris Botzkurt, Ioannis Germanakis, Yannis Stylianou',
+                    year='2018',
+                    url='https://www.sciencedirect.com/science/article/abs/pii/S0010482518301744'
+                 ))
+
+    art_13 = Fact('article-son-seg-rnn',
+                 Article(
+                    name='Sound event aware environmental sound segmentation with Mask U-Net',
+                    authors='Y Sudo, K Itoyama, K Nishida, K Nakadia',
+                    year='2020',
+                    url='https://www.tandfonline.com/doi/abs/10.1080/01691864.2020.1829040'
+                 ))
+
     beta_high = Fact('parametre-beta-grand')
     beta_low = Fact('parametre-beta-petit')
 
